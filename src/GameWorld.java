e05a5dcbc21ea7ca548f7dd0c1168e0c1bd597f9
Williej926 <williej926@gmail.com>
2018-05-11 10:11:16 -0700

    Added deletion code
    When obstacles go offscreen, they are deleted.

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -1,22 +1,32 @@
 import java.util.ArrayList;
+import java.util.List;
 
 import javafx.animation.AnimationTimer;
-import javafx.collections.ObservableList;
 import javafx.scene.Node;
-import javafx.scene.layout.Pane;
 public class GameWorld extends World{
     @Override
     public void act(long now) {
+        System.out.println(getChildren());
+        ArrayList<Node> deletes = new ArrayList();
         for (Node n : getChildren()) {
             if (n instanceof Actor){
-                if (n.getX() > getWorld().getWidth()) {
-                    System.out.println("deleted");
-                    getWorld().remove(n);
-                } else if (n.getX() < 0) {
-                    System.out.println("deleted");
-                    getWorld().remove(n);
+                if (((Actor) n).getX() > ((Actor) n).getWorld().getWidth()) {
+                    //System.out.println("deleted");
+                    //((Actor) n).getWorld().remove(((Actor) n));
+                    deletes.add(((Actor) n));
+                } else if (((Actor) n).getX() < 0) {
+                    //System.out.println("deleted");
+                    //((Actor) n).getWorld().remove(((Actor) n));
+                    deletes.add(((Actor) n));
                 }
             }
         }
+        for(Node n:deletes) {
+            if(n instanceof Actor) {
+                System.out.println("Deleted");
+                ((Actor) n).getWorld().remove(((Actor) n));
+            }
+        }
+
     }
 }
--- src/Obstacles.java
+++ src/Obstacles.java
@@ -1,6 +1,9 @@
 public class Obstacles extends Actor{
     public Obstacles() {
         this.setRotate(0);
+
+    }
+    public Obstacles(Double angle,Double x, Double y) {
     }
     @Override
     public void act(long now) {
